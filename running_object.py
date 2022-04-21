74d324a04ab1275a0b0b03cc5485e5e0f6fcaa3c
Matteo Defranchis <matteo.defranchis@cern.ch>
2022-04-21 17:42:09 +0200

    scale uncertainties

--- running_object.py
+++ running_object.py
@@ -18,15 +18,23 @@ class running_object():
         self.exp_err = np.array([var.err_xsec_1_up/2.+var.err_xsec_1_down/2.,var.err_xsec_2_up/2.+var.err_xsec_2_down/2.,
                     var.err_xsec_3_up/2.+var.err_xsec_3_down/2.,var.err_xsec_4_up/2.+var.err_xsec_4_down/2])*self.exp_xsec/100.
         self.nBins = self.exp_xsec.shape[0]
-        self.d_xsec_vs_mass = self.readAllXsecVsMass(infile_xsec_mass)
+        self.d_xsec_vs_mass, self.d_xsec_vs_mass_scaleup, self.d_xsec_vs_mass_scaledown = self.readAllXsecVsMass(infile_xsec_mass)
         self.d_numunc = self.readNumericalUncertJSON(infile_num_unc)
         self.d_mass_results = self.getAllMasses()
+        self.estimateScaleUncertainties()
+        self.printMassResults()
         
     def clone(self):
         tmp = copy.deepcopy(self)
         tmp.isClone = True
         return tmp
-            
+
+    def update(self):
+        self.d_mass_results = self.getAllMasses()
+        if not self.isClone:
+            self.estimateScaleUncertainties()
+        return
+
     def readAllXsecVsMass(self,filename):
         bin_low = []
         f = open(filename)
@@ -38,16 +46,24 @@ class running_object():
             if not low in bin_low:
                 bin_low.append(low)
         bin_low.sort()
-        d = {}
+        d = dict()
+        d_up = dict()
+        d_down = dict()
         for i, low in enumerate(bin_low):
             m_xsec = dict()
+            m_xsec_up = dict()
+            m_xsec_down = dict()
             for l in lines:
                 if l == '': continue
                 if l.replace(' ','').startswith('#'): continue
                 if int(l.split()[0]) != low: continue
                 m_xsec[l.split()[-1]] = float(l.split()[2])
+                m_xsec_down[l.split()[-1]] = float(l.split()[3])
+                m_xsec_up[l.split()[-1]] = float(l.split()[4])
             d[i]=m_xsec
-        return d
+            d_up[i]=m_xsec_up
+            d_down[i]=m_xsec_down
+        return d, d_up, d_down
 
     def readNumericalUncertJSON(self,filename):
         f = open(filename)
@@ -106,10 +122,9 @@ class running_object():
         l_down.Draw('same')
         c.SaveAs('{}/xsec_bin_{}.png'.format(od,mbin+1))
 
-        m = f.GetX(self.exp_xsec[mbin],120,180)
-        err_up = f.GetX(self.exp_xsec[mbin]-self.exp_err[mbin],120,180) - m
-        err_down = m - f.GetX(self.exp_xsec[mbin]+self.exp_err[mbin],120,180)
-        print ('fit in bin {}: mt(mu) = {:.2f} + {:.2f} - {:.2f} GeV'.format(mbin,m,err_up,err_down))
+        m = f.GetX(self.exp_xsec[mbin],100,200)
+        err_up = f.GetX(self.exp_xsec[mbin]-self.exp_err[mbin],100,200) - m
+        err_down = m - f.GetX(self.exp_xsec[mbin]+self.exp_err[mbin],100,200)
 
         result = self.fillMassResult(m,err_up,err_down)
         
@@ -122,3 +137,35 @@ class running_object():
         setattr(result,'err_down',err_down)
         setattr(result,'err',max(err_up,err_down))
         return result
+
+    def estimateScaleUncertainties(self):
+        obj_up = self.clone()
+        obj_up.d_xsec_vs_mass = self.d_xsec_vs_mass_scaleup
+        obj_up.update()
+        obj_down = self.clone()
+        obj_down.d_xsec_vs_mass = self.d_xsec_vs_mass_scaledown
+        obj_down.update()
+        self.addScaleUncertaintyToResult(obj_up,obj_down)
+        return
+
+    def addScaleUncertaintyToResult(self,obj_up,obj_down):
+        for mbin in range(0,self.nBins):
+            scale_up = abs(self.d_mass_results[mbin].value-obj_down.d_mass_results[mbin].value)
+            scale_down = abs(self.d_mass_results[mbin].value-obj_down.d_mass_results[mbin].value)
+            setattr(self.d_mass_results[mbin],'scale_up',scale_up)
+            setattr(self.d_mass_results[mbin],'scale_down',scale_down)
+            setattr(self.d_mass_results[mbin],'scale',max(scale_up,scale_down))
+        return
+
+    def printMassResults(self,detailed=False):
+        for mbin in range(0,self.nBins):
+            if detailed:
+                print ('bin {}: mt(mu) = {:.2f} + {:.2f} - {:.2f} (exp) + {:.2f} - {:.2f} (scale)'
+                       .format(mbin+1,self.d_mass_results[mbin].value,
+                               self.d_mass_results[mbin].err_up,self.d_mass_results[mbin].err_down,
+                               self.d_mass_results[mbin].scale_up,self.d_mass_results[mbin].scale_down))
+            else:
+                print ('bin {}: mt(mu) = {:.2f} +/- {:.2f} (exp) +/- {:.2f} (scale)'
+                       .format(mbin+1,self.d_mass_results[mbin].value,
+                               self.d_mass_results[mbin].err,self.d_mass_results[mbin].scale))
+        return
